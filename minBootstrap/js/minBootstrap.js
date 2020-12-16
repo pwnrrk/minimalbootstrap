2c26dc45ce137dee1710bcdac9f9173be67ad6fb
R.Phuwanat <ggamegg170@gmail.com>
2020-12-16 12:05:45 +0700

    Modal

--- minBootstrap/js/minBootstrap.js
+++ minBootstrap/js/minBootstrap.js
@@ -1,12 +1,49 @@
 window.onload = ()=>{
     initnavbar()   
+    initmodal()
 }
 
 function initnavbar(){
     document.querySelectorAll('.navbar-toggler').forEach(e => {
-        e.addEventListener('click',()=>{
-            open_navbar(e.dataset.target)
-        })
+        e.addEventListener('click',ev => open_navbar(e.dataset.target))
+    })
+}
+
+function initmodal(){
+    document.querySelectorAll('.btn,.close,.modal').forEach(e => {
+        if (e.classList.contains('modal')){
+            e.addEventListener('click',ev => {
+                if(ev.target == e){
+                    close_modal()
+                }
+            })
+        }
+        if (e.dataset.toggle == 'modal'){
+            e.addEventListener('click', ev => open_modal(e.dataset.target))
+        }
+        if (e.dataset.dismiss == 'modal'){
+            e.addEventListener('click',close_modal)
+        }
+    })
+}
+
+function open_modal(target){
+    document.querySelectorAll(target).forEach(modal=>{
+        modal.classList.add('showing')
+    })
+}
+
+function close_modal(){
+    document.querySelectorAll('.modal').forEach(modal => {
+        if (modal.classList.contains('showing')){
+            modal.classList.add('goingout')
+            modal.addEventListener('animationend',()=>{
+                if(modal.classList.contains('goingout')){
+                    modal.classList.remove('goingout')
+                    modal.classList.remove('showing')
+                }
+            })
+        }
     })
 }
 
